8c25fe55b1e03dc2d83d39ae222a7ca612a107a4
BuiHieu123 <buihieu1262003@gmail.com>
2024-02-02 15:49:55 +0700

    testqlnv

--- DuAn1/src/ModuleTest/QLNV.java
+++ DuAn1/src/ModuleTest/QLNV.java
@@ -4,16 +4,33 @@
  */
 package ModuleTest;
 
+import domainmodel.NhanVienTest;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Admin 88
  */
 public class QLNV {
-   public int searchNVbyName(String name){
-       if(name.equalsIgnoreCase("Nam")){
-           return 1;
-       }
-       return -1;
-   }
-  
+
+    public int searchNVbyName(String name) {
+        if (name.equalsIgnoreCase("Nam")) {
+            return 1;
+        }
+        return -1;
+    }
+
+    public int addNV(NhanVienTest nv) {
+        List<NhanVienTest> lst = new ArrayList<>();
+        lst.add(new NhanVienTest("NV01", "Bui Minh Hieu", "12-6-2003", "Ha Nam", "03572482932", "Đang Làm Việc", "hieubmph21306@fpt.edu.vn", "0218329392", "", "Nhân Viên"));
+        lst.add(new NhanVienTest("NV02", "Bui Minh Hieu1", "12-6-2003", "Ha Nam", "03572482932", "Đang Làm Việc", "hieubmph21307@fpt.edu.vn", "0218329392", "", "Quản Lý"));
+        lst.add(new NhanVienTest("NV03", "Bui Minh Hieu2", "12-6-2003", "Ha Nam", "03572482932", "Đang Làm Việc", "hieubmph21308@fpt.edu.vn", "0218329392", "", "Nhân Viên"));
+        if (nv.getMaNhanVien().equals("") || nv.getHoVaTen().equals("") || nv.getNgaySinh().equals("") || nv.getDiaChi().equals("") || nv.getCCCD().equals("") || nv.getTrangThai().equals("") || nv.getEmail().equals("") || nv.getSoDienThoai().equals("") || nv.getChucVu().equals("")) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
 }
--- DuAn1/test/TestCase/QLNVTest.java
+++ DuAn1/test/TestCase/QLNVTest.java
@@ -1,10 +1,11 @@
-/*
+ /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package TestCase;
 
 import ModuleTest.QLNV;
+import domainmodel.NhanVienTest;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -37,5 +38,38 @@ public class QLNVTest {
         int ex = 1;
         assertEquals(ex, test);
     }
+    
+    //Test ADD
+    @Test
+    public void testAddNV01(){
+        int test=qlnv.addNV(new NhanVienTest("NV01", "Bui Minh Hieu", "12-6-2003", "Ha Nam", "03572482932", "Đang Làm Việc", "hieubmph21306@fpt.edu.vn", "0218329392", "", "Nhân Viên"));
+        int ex=1;
+        assertEquals(ex, test);
+    }
+    @Test
+    public void testAddNV02(){
+        int test=qlnv.addNV(new NhanVienTest("NV01", "", "12-6-2003", "Ha Nam", "03572482932", "Đang Làm Việc", "hieubmph21306@fpt.edu.vn", "0218329392", "", "Nhân Viên"));
+        int ex=1;
+        assertEquals(ex, test);
+    }
+    @Test
+    public void testAddNV03(){
+        int test=qlnv.addNV(new NhanVienTest("NV02", "Bui Minh Hieu1", "", "Ha Nam", "03572482932", "Đang Làm Việc", "hieubmph21306@fpt.edu.vn", "0218329392", "", "Nhân Viên"));
+        int ex=1;
+        assertEquals(ex, test);
+    }
+    @Test
+    public void testAddNV04(){
+        int test=qlnv.addNV(new NhanVienTest("NV03", "Bui Minh Hieu", "12-6-2003", "", "03572482932", "Đang Làm Việc", "hieubmph21306@fpt.edu.vn", "0218329392", "", "Nhân Viên"));
+        int ex=1;
+        assertEquals(ex, test);
+    }
+    @Test
+    public void testAddNV05(){
+        int test=qlnv.addNV(new NhanVienTest("NV01", "Bui Minh Hieu", "12-6-2003", "Ha Nam", "", "Đang Làm Việc", "hieubmph21306@fpt.edu.vn", "0218329392", "", "Nhân Viên"));
+        int ex=1;
+        assertEquals(ex, test);
+    }
+  
     
 }
